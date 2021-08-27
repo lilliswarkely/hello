87de8e0a674f81bc859a2b06033aed15e8bd0b4b
lilliswarkley <lwillis17@huskers.unl.edu>
2021-08-27 15:54:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Logan 'luna' willis
+* Date: 2021/09/08/24
+*
+* A simple hello world program in C
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Hello Planet Earth!\n");
+
+return 0;
+}
